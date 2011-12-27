f2413f9b09943d4ea4eace840563f508b0b392bb
Ryan Rix <ry@n.rix.si>
2011-12-26 20:27:23 -0700

    Wow, I'm an idiot.
    I was parsing the output of the OAC incorrectly. >.<

--- source/HslLock.js
+++ source/HslLock.js
@@ -25,7 +25,7 @@ enyo.kind({
 
     updateColor: function() {
         var color;
-        if(this.currentStatus == "l"){
+        if(this.currentStatus == "1"){
             this.$.lockedButton.setState("depressed", true);
             this.$.unlockedButton.setState("depressed", false);
             color = "red";
@@ -45,19 +45,24 @@ enyo.kind({
         if( inRequest.params.cmd == "status" ) {
             var results = inResponse;
             var lines = results.split("\r");
-            for (i = 0; i< lines.length; i++){
-                var line = lines[i];
-                var lockStatus = line.match(/[0-9]$/g);
-                var lockStatus2 = line.match(/[0-9]$/g);
-                if(lockStatus2) {
-                    lockStatus = lockStatus2;
-                }
-                if(lockStatus) {
-                    lockStatus = lockStatus[0].toString()
+            
+            line1 = lines[lines.length - 3];
+            line2 = lines[lines.length - 2];
+            
+            enyo.log(line1)
+            enyo.log(line2)
+            
+            var lockStatus = line1.match(/[0-9]$/g);
+            var lockStatus2 = line2.match(/[0-9]$/g);
+            
+            if(lockStatus2) {
+                lockStatus = lockStatus2;
+            }
+            if(lockStatus) {
+                lockStatus = lockStatus[0].toString()
 
-                    this.currentStatus = lockStatus;
-                    this.updateColor();
-                }
+                this.currentStatus = lockStatus;
+                this.updateColor();
             }
         }
 
