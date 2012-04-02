652509299830cfe4556c3aca13b8271cfb558d68
Ryan Rix <ry@n.rix.si>
2012-04-02 00:37:29 -0700

    T: Store login data to html5 localStorage

--- source/LoginPanel.js
+++ source/LoginPanel.js
@@ -6,18 +6,28 @@ enyo.kind( {
   floating: true,
 
   components: [
-    { kind: "onyx.Input",
-      name: "userInput",
-      placeholder: "Username" },
-    { kind: "onyx.Input",
-      name: "passwordInput",
-      placeholder: "Password" },
+    { kind: "onyx.InputDecorator", components: [
+      { kind: "onyx.Input",
+        name: "userInput",
+        placeholder: "Username" },
+    ]},
+    { kind: "onyx.InputDecorator", components: [
+      { kind: "onyx.Input",
+        name: "passwordInput",
+        placeholder: "Password" },
+    ]},
     { kind: "onyx.Button",
       content: "Save",
       onclick: "saveData" }
   ],
 
   saveData: function() {
+    enyo.log("clicK");
+    values = new Array();
+    values.push(this.$.userInput.hasNode().value);
+    values.push(this.$.passwordInput.hasNode().value);
 
+    localStorage.setItem("hsllock_loginData", values.join("|") );
+    this.hide();
   }
 });
