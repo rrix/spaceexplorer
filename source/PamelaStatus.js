6792956d7716d70fbefb78f609ba7401e8f101dd
Ryan Rix <ry@n.rix.si>
2012-04-01 23:51:29 -0700

    T: PamelaStatus is the workings!

--- source/PamelaStatus.js
+++ source/PamelaStatus.js
@@ -1,28 +1,43 @@
 enyo.kind( {
-  kind: "enyo.Component",
+  kind: "enyo.Control",
   name: "HslLocks.PamelaStatus",
+  style: "text-align: center",
 
   components: [
-    { kind: "h1",
+    { tag: "h1",
       content: "Who's in the Space?" },
-    { kind: "div",
-      name: "peopleDiv" }
+    { tag: "ul",
+      name: "peopleUl" }
   ],
 
   create: function() {
     this.inherited(arguments);
 
+    this.pamelaItems = [];
+
     this.pamelaAjax = new enyo.Ajax( { url: "http://172.22.110.17/data.php" } );
     this.updateFromPamela();
-  }
+  },
 
   updateFromPamela: function() {
     this.pamelaAjax.response( enyo.bind(this, this.updateResponse) );
     this.pamelaAjax.go();
-  }
+  },
 
-  updateResponse: function(inResponse, inRequest) {
-    enyo.log(inResponse);
-  }
+  updateResponse: function(inRequest, inResponse) {
+    this.rebuild(inResponse);
+  },
 
-} );
+  rebuild: function( data ) {
+    this.$.peopleUl.destroyComponents();
+
+    for( var i = 0; i < data.length; i++) {
+      if( !/^\./.exec(data[i]) ) {
+        this.pamelaItems[i] = this.$.peopleUl.createComponent({tag: "li", content: data[i]} );
+      }
+    }
+
+    this.render();
+    setTimeout( enyo.bind(this, this.updateFromPamela()), 30000 );
+  }
+});
