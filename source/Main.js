711c21d53bd6599d879beb9b467d12118cbcd827
Ryan Rix <ry@n.rix.si>
2012-07-07 14:13:13 -0700

    Play with data API

--- source/Main.js
+++ source/Main.js
@@ -2,5 +2,20 @@ enyo.kind({
   name: "SpaceAPI.Main",
   fit: true,
   components:[
+    {
+      kind: "SpaceAPI.Spaces",
+      name: "spaces",
+      onSpacesFetched: "spacesFetched"
+    }
   ],
+
+  create: function() {
+    this.inherited(arguments);
+
+    this.$.spaces.go();
+  },
+
+  spacesFetched: function( inSender ) {
+    enyo.log(this.$.spaces.spaceStatuses);
+  }
 });
