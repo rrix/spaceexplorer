245315e61604ae8bea5ab1e967cacac40e93098d
Ryan Rix <ry@n.rix.si>
2012-07-07 14:13:06 -0700

    Full data API complete

--- source/Spaces.js
+++ source/Spaces.js
@@ -3,14 +3,110 @@ enyo.kind({
   name: "SpaceAPI.Spaces",
 
   published: {
-    directory: "http://chasmcity.sonologic.nl/spacestatusdirectory.php",
+    directory: "http://openspace.slopjong.de/directory.json",
+    directoryData: {},
+    state:    "idle",
+    spaceStatuses: {},
+    spaceCount: 0
   },
 
   events: {
+    onDirectoryFetched: "",
     onSpaceFetched: "",
-    onSpacesFetched: ""
+    onSpacesFetched: "",
+    onFetchError: ""
   },
 
+  fetchedSpaceCount: 0,
+
   go: function() {
+    var directoryFetch = new enyo.Ajax( { url: this.directory } );
+    directoryFetch.go();
+    this.state = "directoryFetch";
+
+    directoryFetch.response( this, "directoryFetched"      );
+    directoryFetch.error(    this, "directoryFetchFailure" );
+  },
+
+  directoryFetched: function( inSender, inResponse ) {
+    this.state = "directoryFetched";
+    this.directoryData = inResponse;
+
+    this.doDirectoryFetched( inResponse );
+    this.fetchSpaces( inResponse );
+  },
+
+  directoryFetchFailure: function( inSender, inResponse ) {
+    this.state = "directoryFetchFailed";
+    doFetchError();
+  },
+
+  fetchSpaces: function( directoryJson ) {
+    delete directoryJson['originator'];
+
+    // FIXME: http://stackoverflow.com/questions/5223/length-of-javascript-object-ie-associative-array
+    var size = function(obj) {
+      var size = 0, key;
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+      }
+      return size;
+    };
+
+    this.spaceCount = size(directoryJson);
+
+    this.state = "spacesFetch";
+
+    for( space in directoryJson ) {
+      // Filter originator being added to emitted events
+      var url = directoryJson[space];
+      if( typeof( url ) == "string" ) {
+        // FIXME: techinc has a bad SSL cert
+        if( url.match(/techinc.nl/) )
+        {
+          url.replace("https", "http");
+          directoryJson[url] = directoryJson[space];
+          delete directoryJson[space];
+        }
+        this.fetchSpace( url );
+      }
+    }
+  },
+
+  fetchSpace: function( spaceURL ) {
+    if( typeof( spaceURL ) == "string") {
+      var spaceFetch = new enyo.Ajax( { url: spaceURL } );
+      this.state = "spaceFetch";
+
+      spaceFetch.response( this, "spaceFetched"      );
+      spaceFetch.error(    this, "spaceFetchFailure" );
+      spaceFetch.go();
+    }
+  },
+
+  spaceFetched: function( inSender, inResponse ) {
+    var fail = false;
+
+    if( typeof(inResponse) == "object" ) {
+      this.spaceStatuses[inSender.url] = inResponse;
+      this.doSpaceFetched({url: inSender.url, response: inResponse});
+    } else {
+      fail = true;
+    }
+
+    this.fetchedSpaceCount++;
+    if( this.fetchedSpaceCount == this.spaceCount ) {
+      this.state = "spacesFetched";
+      this.doSpacesFetched();
+    }
+
+    if(fail) {
+      inSender.fail(inResponse);
+    }
+  },
+
+  spaceFetchFailure: function(inSender, inResponse) {
+    this.spaceStatuses[inSender.url] = {error: "Fetch returned undefined"};
+    this.doFetchError(inSender, inSender.url);
   }
-}
+});
