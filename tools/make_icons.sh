66e3f736897cca5328f02d2e5bc394d11b9ae5cf
Ryan Rix <ry@n.rix.si>
2012-08-30 01:32:01 -0700

    Update deploy scripts

--- tools/make_icons.sh
+++ tools/make_icons.sh
@@ -1,7 +1,7 @@
 mkdir -p ./assets/icons
 
 for size in 36 48 57 64 72 72 90 96 114 512; do
-  echo generating $size ...
+  echo Generating $size pixel icon ...
   inkscape -D -e ./assets/icons/appicon-$size.png -w $size -h $size -f ./source/appicon.svg &>/dev/null
 done
 
