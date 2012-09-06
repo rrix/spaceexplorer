a3d0eb748ea3c779b5eff7cffa7eed88ce4d00da
Ryan Rix <ry@n.rix.si>
2012-09-06 15:37:37 -0700

    Refactor a lot of the HslLock code in to something cleaner and legible

--- source/HslLock.js
+++ source/HslLock.js
@@ -1,13 +1,14 @@
-/* vim: set foldmethod=indent foldlevel=1: */
+/* vim: set foldmethod=marker foldlevel=0: */
 enyo.kind({
   name: "HslLocks.Main",
   layoutKind: "FittableRowsLayout",
 
   published: {
-    spaceAPIEndpoint: "http://intranet.heatsynclabs.org/~access/cgi-bin/spaceapi.rb"
+    spaceAPIEndpoint: "http://intranet.heatsynclabs.org/~access/cgi-bin/spaceapi.rb",
+    space: {}
   },
 
-  components: [
+  components: [ // {{{1
     {
       kind: "HslLocks.Buttons",
       onclick: "lockGroupClick",
@@ -16,12 +17,12 @@ enyo.kind({
     {
       tag: "h1",
       content: "Who's in the Space?",
-      style: 'text-align: center;'
+      style: 'text-align: center;color: white'
     },
     {
       kind: "HslLocks.PamelaStatus",
       name: "pamelaStatus",
-      style: "height: 69%"
+      style: "height: 69%; color:white"
     },
     {
       name: 'loginButton',
@@ -42,6 +43,7 @@ enyo.kind({
       onLoginChanged: "loadLoginData"
     }
   ],
+  // }}}1
 
   rendered: function() {
     this.inherited(arguments);
@@ -54,22 +56,30 @@ enyo.kind({
 
     // This refreshes the screen every 30 seconds
     setInterval( enyo.bind(this, this.getCurrentStatus), 30000);
-
-    // In webOS this makes the loading screen go away
-    // FIXME: Can we make this go away with phonegap.js?
-    if( window.PalmSystem ) {
-      window.PalmSystem.stageReady();
-    }
   },
 
-  hideButtons: function() {
-    this.$.lockGroup.setShowing(false);
+  // OAC Functionality {{{1
+  // This checks our OAC login.
+  validateData: function(url, username, password, callback) {
+    var apiAjax = new enyo.Ajax( { url: url } );
+
+    apiAjax.response( enyo.bind(this, function(inSender, inResponse) {
+      var loginCheck = new enyo.Ajax( { url: inResponse.apis.oac.url } );
+      loginCheck.response( enyo.bind(this, function(inSender, inResponse) {
+        if( inResponse.login == 'okay' ) {
+          callback(true, url, username, password);
+        } else {
+          callback(false, url, username, password);
+        }
+      }));
+
+      loginCheck.go({ cmd: "check-login", user: username, pass: password });
+    }));
+
+    apiAjax.go();
   },
 
-  showButtons: function() {
-    this.$.lockGroup.setShowing(true);
-  },
-
+  // This loads our login data from localStorage
   loadLoginData: function() {
     // Pull login data from HTML5 localStorage, open the popup if it doesn't
     // exist.
@@ -88,32 +98,68 @@ enyo.kind({
     }
 
     this.statusAjaxEndpoint = new enyo.Ajax({ url: this.spaceAPIEndpoint});
-    this.statusAjaxEndpoint.response(this, enyo.bind(this, function(inSender, inResponse) {
-      this.url = inResponse.apis.oac.url;
-      this.spaceAPIData = inResponse;
-      this.$.pamelaStatus.setUrl( this.spaceAPIData.apis.pamela.url );
-
-      this.statusRetrieved(inSender, inResponse);
-    }));
+    this.statusAjaxEndpoint.response(this, enyo.bind(this, "loadSpaceApiResponse"));
 
     this.statusAjaxEndpoint.go();
   },
+
+  loadSpaceApiResponse: function(inSender, inResponse) {
+    if (inResponse.apis && inResponse.apis.oac ) {
+      this.oacUrl = inResponse.apis.oac.url;
+    } else {
+      this.oacUrl = undefined;
+    }
+    this.spaceAPIData = inResponse;
+    if (this.spaceAPIData.apis && this.spaceAPIData.apis.pamela ) {
+      this.$.pamelaStatus.setUrl( this.spaceAPIData.apis.pamela.url );
+    } else {
+      this.$.pamelaStatus.setUrl( undefined );
+    }
+
+    this.statusRetrieved(inSender, inResponse);
+  },
+
+  // FIXME: needs a better name.
+  sendToLock: function() {
+    this.$.scrim.show();
+    this.currentStatus = this.$.lockGroup.value;
+    this.lockAjaxEndpoint = new enyo.Ajax({url: this.oacUrl});
+    this.lockAjaxEndpoint.go({user: this.username, pass: this.password, cmd: this.$.lockGroup.value});
+    this.lockAjaxEndpoint.handleAs = "text";
+    this.jamLock = false;
+
+    // This is a band-aid, yay band-aids
+    // Basically, there's a delay between when OAC unlocks and when it
+    // admits it's unlocked. This will help make that less noticeable.
+    setTimeout( enyo.bind(this,this.getCurrentStatus), 10000);
+  },
+  // }}}1
+
+  hideButtons: function() {
+    this.$.lockGroup.setShowing(false);
+  },
+
+  showButtons: function() {
+    this.$.lockGroup.setShowing(true);
+  },
+
 
   /*
    * This function handles updating the UI based on the currentStatus
    */
   updateColor: function() {
-    var color;
     if(this.currentStatus == "1"){
       this.$.lockGroup.locked();
-      color = "red";
+      this.addClass("closedSpace");
+      this.removeClass("openSpace");
     } else {
       this.$.lockGroup.unlocked();
-      color = "green";
+      this.addClass("openSpace");
+      this.removeClass("closedSpace");
     }
-    document.body.style.backgroundColor = color;
   },
 
+  // Status retrieval {{{1
   getCurrentStatus: function() {
     // FIXME: Move this to create somehow
     this.statusAjaxEndpoint = new enyo.Ajax({ url: this.spaceAPIEndpoint});
@@ -136,7 +182,7 @@ enyo.kind({
     this.updateColor();
 
     this.$.scrim.hide();
-  },
+  }, // }}}1
 
   lockGroupClick: function(inSender, e) {
     if( this.$.lockGroup.value == "toggle" )
@@ -155,42 +201,13 @@ enyo.kind({
     }
   },
 
-  // FIXME: needs a better name.
-  sendToLock: function() {
-    this.$.scrim.show();
-    this.currentStatus = this.$.lockGroup.value;
-    this.lockAjaxEndpoint = new enyo.Ajax({url: this.url});
-    this.lockAjaxEndpoint.go({user: this.username, pass: this.password, cmd: this.$.lockGroup.value});
-    this.lockAjaxEndpoint.handleAs = "text";
-    this.jamLock = false;
-
-    // This is a band-aid, yay band-aids
-    // Basically, there's a delay between when OAC unlocks and when it
-    // admits it's unlocked. This will help make that less noticeable.
-    setTimeout( enyo.bind(this,this.getCurrentStatus), 10000);
-  },
-
   showPopup: function() {
     this.$.loginPopup.show();
   },
 
-  validateData: function(url, username, password, callback) {
-    var apiAjax = new enyo.Ajax( { url: url } );
-
-    apiAjax.response( enyo.bind(this, function(inSender, inResponse) {
-      var loginCheck = new enyo.Ajax( { url: inResponse.apis.oac.url } );
-      loginCheck.response( enyo.bind(this, function(inSender, inResponse) {
-        if( inResponse.login == 'okay' ) {
-          callback(true, url, username, password);
-        } else {
-          callback(false, url, username, password);
-        }
-      }));
-
-      loginCheck.go({ cmd: "check-login", user: username, pass: password });
-    }));
-
-    apiAjax.go();
+  setSpace: function(newdata) {
+    this.loadSpaceApiResponse(undefined, newdata);
+    this.inherited(arguments);
   }
 
 });
